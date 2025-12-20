37d31076968c6fe8bb06285e5e93a5da90f124ca
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-12-20 20:30:01 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+function App() {
+  return (
+    <div>
+      {/* Conteúdo será adicionado aqui */}
+      <Router>
+        <Routes>
+        </Routes>  
+      </Router>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-yellow-300 text-black py-4 mt-10">
+      <p className="text-center text-sm">
+        ©2025
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
