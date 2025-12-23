36c9dd084367be8c5df74e1304028b65e83fac4a
Leocassio Silva <56235626+leocassiosilva@users.noreply.github.com>
2025-12-22 21:02:35 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+
+
 function App() {
   return (
     <div>
       {/* Conteúdo será adicionado aqui */}
+      <Navbar />
       <Router>
         <Routes>
         </Routes>  
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <nav className="bg-yellow-300 shadow-md fixed w-full top-0 z-50">
+            <div className="max-w-6xl mx-auto px-4">
+                    <h2>Get  A Pet</h2>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
